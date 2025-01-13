198f48564c26097cb0ad0c19089afaf23f40013e
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2025-01-13 16:23:27 +0600

    created routes for editing the tasks of an authorized user and also deleting them

--- api/routes/tasks/tasks.ts
+++ api/routes/tasks/tasks.ts
@@ -18,7 +18,7 @@ tasksRouter.get("/", auth, async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-})
+});
 
 tasksRouter.post("/", auth, async (req, res, next) => {
     try {
@@ -46,6 +46,79 @@ tasksRouter.post("/", auth, async (req, res, next) => {
             res.status(400).send({ error: error.message, details: error.errors });
             return;
         }
+        next(error);
+    }
+});
+
+tasksRouter.put("/:id", auth, async (req, res, next) => {
+    try {
+        const user = (req as RequestWithUser).user;
+        if (!user) {
+            res.status(401).send({ error: "Unauthorized" });
+            return;
+        }
+
+        const { id } = req.params;
+        const { title, description, status } = req.body;
+
+        const possibleStatuses = ["new", "complete", "in_progress"];
+        if (status && !possibleStatuses.includes(status)) {
+            res.status(400).send({ error: "Possible statuses: new, complete and in_progress!" });
+            return;
+        }
+
+        const task = await Task.findOne({ _id: id });
+        if (!task) {
+            res.status(404).send({ error: "Task not found" });
+            return;
+        }
+
+        if (task.user.toString() !== user._id.toString()) {
+            res.status(403).send({ error: "Forbidden: You are not allowed to edit this task" });
+            return;
+        }
+
+        if (title) task.title = title;
+        if (description) task.description = description;
+        if (status) task.status = status;
+
+        await task.save();
+
+        res.status(200).send(task);
+    } catch (error) {
+        if (error instanceof Error.ValidationError) {
+            res.status(400).send({ error: error.message, details: error.errors });
+            return;
+        }
+        next(error);
+    }
+});
+
+tasksRouter.delete("/:id", auth, async (req, res, next) => {
+    try {
+        const user = (req as RequestWithUser).user;
+        if (!user) {
+            res.status(401).send({ error: "Unauthorized" });
+            return;
+        }
+
+        const { id } = req.params;
+
+        const task = await Task.findOne({ _id: id });
+        if (!task) {
+            res.status(404).send({ error: "Task not found" });
+            return;
+        }
+
+        if (task.user.toString() !== user._id.toString()) {
+            res.status(403).send({ error: "Forbidden: You are not allowed to delete this task" });
+            return;
+        }
+
+        await task.deleteOne();
+
+        res.status(200).send({ message: "Task deleted successfully" });
+    } catch (error) {
         next(error);
     }
 });
